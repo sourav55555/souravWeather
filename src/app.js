f95c9ba9a6849f4abe4e2cdde112580337d4dc1f
sourav55555 <souravsinghassr555@gmail.com>
2021-07-02 20:40:01 +0600

    addding new script

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const app= express();
 const path= require('path');
-const port=3000;
+const port=process.env.PORT || 5000;
 const hbs= require('hbs');
 
 //static
